6d61b693b0654bbbbe28bdc685ff84d4f049f98f
nawaf-al-hussain <nkhondokar2420136@bscse.uiu.ac.bd>
2026-02-28 17:45:46 +0100

    Fix the health check including it to apiRouter remove Router from server

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -4,7 +4,21 @@ import mapRouter from "./map.routes.ts";
 import playerRouter from "./player.routes.ts";
 import docsRouter from "./docs.routes.ts";
 
+const clientPort = Deno.env.get("CLIENT_PORT") || "8080";
+const serverPort = Deno.env.get("SERVER_PORT") || "3000";
+
+
 const apiRouter = new Router({ prefix: "/api/v1" });
+
+apiRouter.get("/", (ctx) => {
+  ctx.response.body = {
+    message: "Archi-Fighter API is running!",
+    status: "running",
+    client: `http://localhost:${clientPort}`,
+	docs: `http://localhost:${serverPort}/api/v1/docs`
+
+  };
+});
 
 apiRouter.use(characterRouter.routes(), characterRouter.allowedMethods());
 apiRouter.use(mapRouter.routes(), mapRouter.allowedMethods());
--- server/server.ts
+++ server/server.ts
@@ -5,25 +5,11 @@ import { runMigrations } from "./database/migrations.ts";
 import apiRouter from "./routes/index.ts";
 import docsRouter from "./routes/docs.routes.ts";
 
-
-const router = new Router();
 const app = new Application();
 
-const clientPort = Deno.env.get("CLIENT_PORT") || "8080";
 const serverPort = Deno.env.get("SERVER_PORT") || "3000";
 
-// Root endpoint
-router.get("/", (ctx) => {
-  ctx.response.body = {
-    message: "Archi-Fighter Server",
-    status: "running",
-    client: `http://localhost:${clientPort}`,
-
-  };
-});
-
 // Register router middleware
-app.use(router.routes());       // GET /
 app.use(docsRouter.routes());   // GET /docs, GET /openapi.json
 app.use(apiRouter.routes());    // GET /api/v1/characters, etc.
 
